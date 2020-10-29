4832f6061abf739fb34bbca78741be8d93b0c05e
Đoàn Quang Hòa <hoa9x3@gmail.com>
2020-10-29 14:03:10 +0700

    Update SecurityConfiguration.java

--- backend/lab3/src/main/java/com/techmaster/configuration/SecurityConfiguration.java
+++ backend/lab3/src/main/java/com/techmaster/configuration/SecurityConfiguration.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// Declare 
 @Configuration
 public class SecurityConfiguration implements WebMvcConfigurer {
 
+    // Declare bean BCryptPasswordEncoder which load to spring Context when application run success
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
