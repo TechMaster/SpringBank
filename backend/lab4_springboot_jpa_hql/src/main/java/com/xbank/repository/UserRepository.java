ef7e7ff91723087e2991325e399a0b75257fe426
Quanghoa <hoaronal-project>
2020-11-14 00:06:16 +0700

    hql

--- backend/lab4_springboot_jpa_hql/src/main/java/com/xbank/repository/UserRepository.java
+++ backend/lab4_springboot_jpa_hql/src/main/java/com/xbank/repository/UserRepository.java
@@ -2,11 +2,7 @@ package com.xbank.repository;
 
 import com.xbank.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, CustomUserRepository {
 
-    @Query("SELECT user FROM User user WHERE user.fullName = :fullName")
-    User searchUserByFullName(@Param("fullName") String fullName);
 }
