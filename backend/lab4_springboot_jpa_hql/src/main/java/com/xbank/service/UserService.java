c1e1ba93d47b7c7a674369af8a8fa5f4241d6fc4
Quanghoa <hoaronal-project>
2020-11-13 22:52:44 +0700

    HQL

--- backend/lab4_springboot_jpa_hql/src/main/java/com/xbank/service/UserService.java
+++ backend/lab4_springboot_jpa_hql/src/main/java/com/xbank/service/UserService.java
@@ -1,17 +1,11 @@
 package com.xbank.service;
 
 import com.xbank.domain.User;
-import com.xbank.dto.UserDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import com.xbank.repository.UserRepository;
-
-import javax.persistence.EntityNotFoundException;
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 
 @Slf4j
 @Service
@@ -20,46 +14,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    public List<User> findAll(){
-        return userRepository.findAll();
-    }
-
     public User getUserByFullName(String fullName){
         return userRepository.searchUserByFullName(fullName);
     }
-
-    public User createUser(UserDTO dto) {
-        User user = new User();
-        if (Objects.nonNull(dto)) {
-            user.setFullName(dto.getFullName());
-            user.setEmail(dto.getEmail());
-            user.setMobile(dto.getMobile());
-            user.setPhoto(dto.getPhoto());
-        }
-        return userRepository.save(user);
-    }
-
-    public User updateUser(UserDTO dto, long id) {
-        Optional<User> userOptional = userRepository.findById(id);
-        if (userOptional.isPresent()){
-            User user = userOptional.get();
-            user.setFullName(dto.getFullName());
-            user.setEmail(dto.getEmail());
-            user.setMobile(dto.getMobile());
-            user.setPhoto(dto.getPhoto());
-            return userRepository.save(user);
-        }
-        return null;
-    }
-
-    public long deleteById(long id){
-        try {
-            userRepository.deleteById(id);
-            return id;
-        } catch (Exception e) {
-            log.error("Error when delete user {}", id, e);
-        }
-        throw new EntityNotFoundException();
-    }
-
 }
