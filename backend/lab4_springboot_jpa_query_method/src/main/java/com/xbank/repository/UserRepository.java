6bf797f19f4fdf73d6e7029d847c62088e6c9b1a
Quanghoa <hoaronal-project>
2020-11-13 23:02:27 +0700

    query method

--- backend/lab4_springboot_jpa_query_method/src/main/java/com/xbank/repository/UserRepository.java
+++ backend/lab4_springboot_jpa_query_method/src/main/java/com/xbank/repository/UserRepository.java
@@ -7,12 +7,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    /**
-     * To fetch User by fullName
-     * @param fullName
-     * @return
-     */
-    @Query("SELECT user FROM User user WHERE user.fullName = :fullName")
-    User searchUserByFullName(@Param("fullName") String fullName);
+    User findByFullName(@Param("fullName") String fullName);
 
 }
