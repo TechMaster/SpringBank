a7490b9b1751cc68de354103bb37f965c61e5f8e
Quanghoa <hoaronal-project>
2020-11-13 23:18:44 +0700

    Native query

--- backend/lab4_springboot_jpa_native_query/src/main/java/com/xbank/repository/UserRepository.java
+++ backend/lab4_springboot_jpa_native_query/src/main/java/com/xbank/repository/UserRepository.java
@@ -5,14 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long>, CustomUserRepository {
 
-    /**
-     * To fetch User by fullName
-     * @param fullName
-     * @return
-     */
-    @Query("SELECT user FROM User user WHERE user.fullName = :fullName")
+    @Query(value = "SELECT * FROM USER WHERE FULL_NAME = :fullName", nativeQuery = true)
     User searchUserByFullName(@Param("fullName") String fullName);
 
 }
