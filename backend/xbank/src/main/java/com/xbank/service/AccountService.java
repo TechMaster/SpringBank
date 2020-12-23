0352583692ed89655ba302fc7a6b247885ac446d
Quanghoa <hoaronal-project>
2020-12-24 01:24:13 +0700

    update api account

--- backend/xbank/src/main/java/com/xbank/service/AccountService.java
+++ backend/xbank/src/main/java/com/xbank/service/AccountService.java
@@ -2,12 +2,14 @@ package com.xbank.service;
 
 import com.xbank.config.Constants;
 import com.xbank.domain.Account;
+import com.xbank.domain.Notification;
 import com.xbank.domain.Transaction;
 import com.xbank.dto.AccountDTO;
 import com.xbank.dto.AccountTranferDTO;
 import com.xbank.dto.WithDrawDTO;
 import com.xbank.event.TransactionEvent;
 import com.xbank.repository.AccountRepository;
+import com.xbank.repository.NotificationRepository;
 import com.xbank.repository.TransactionRepository;
 import com.xbank.rest.errors.AccountExitsException;
 import com.xbank.rest.errors.WithdrawException;
@@ -22,11 +24,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import javax.validation.Valid;
 import java.math.BigDecimal;
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -47,11 +47,14 @@ public class AccountService {
 
     private final TransactionRepository transactionRepository;
 
+    private final NotificationRepository notificationRepository;
+
     private final ApplicationEventPublisher publisher;
 
-    public AccountService(AccountRepository accountRepository, TransactionRepository transactionRepository, ApplicationEventPublisher publisher) {
+    public AccountService(AccountRepository accountRepository, TransactionRepository transactionRepository, NotificationRepository notificationRepository, ApplicationEventPublisher publisher) {
         this.accountRepository = accountRepository;
         this.transactionRepository = transactionRepository;
+        this.notificationRepository = notificationRepository;
         this.publisher = publisher;
     }
 
@@ -141,7 +144,7 @@ public class AccountService {
                                     acc1.setBalance(acc1.getBalance().add(data.getBalance()));
                                     return accountRepository.save(acc1);
                                 }));
-                            });
+                            }).doOnSuccess(item -> publishTransactionEvent(TransactionEvent.ITEM_CREATED, transaction));
                         }).map(acc -> {
                             try {
                                 return ResponseEntity.created(new URI("/api/accounts/tranfer" + acc.getId()))
@@ -179,7 +182,7 @@ public class AccountService {
                             return transactionRepository.save(transaction).flatMap(t -> {
                                 account.setBalance(account.getBalance().subtract(data.getBalance()));
                                 return accountRepository.save(account);
-                            });
+                            }).doOnSuccess(item -> publishTransactionEvent(TransactionEvent.ITEM_CREATED, transaction));
                         }).map(acc -> {
                             try {
                                 return ResponseEntity.created(new URI("/api/accounts/withdraw" + acc.getId()))
@@ -214,7 +217,7 @@ public class AccountService {
                             return transactionRepository.save(transaction).flatMap(t -> {
                                 account.setBalance(account.getBalance().add(data.getBalance()));
                                 return accountRepository.save(account);
-                            });
+                            }).doOnSuccess(item -> publishTransactionEvent(TransactionEvent.ITEM_CREATED, transaction));
                         }).map(acc -> {
                             try {
                                 return ResponseEntity.created(new URI("/api/accounts/deposit" + acc.getId()))
@@ -226,7 +229,22 @@ public class AccountService {
                         }));
     }
 
-    private final void publishTransactionEvent(String eventType, Transaction item) {
-        this.publisher.publishEvent(new TransactionEvent(eventType, item));
+    private final void publishTransactionEvent(String eventType, Transaction transaction) {
+        this.publisher.publishEvent(new TransactionEvent(eventType, transaction));
+        Notification notification = new Notification();
+        notification.setAccount(transaction.getToAccount());
+        notification.setCreatedDate(LocalDateTime.now());
+        notification.setLastModifiedDate(LocalDateTime.now());
+        if (transaction.getAction() == 1) {
+            // Tranfer action
+            notification.setTitle(transaction.getAccount() + " has transferred to you " + transaction.getAmount() + " at " + transaction.getTransactAt());
+        } else if (transaction.getAction() == 2) {
+            // withdraw action
+            notification.setTitle("Withdraw " + transaction.getAmount() + " at " + transaction.getTransactAt());
+        } else if (transaction.getAction() == 3) {
+            // deposit action
+            notification.setTitle("Deposit " + transaction.getAmount() + " at " + transaction.getTransactAt());
+        }
+        notificationRepository.save(notification).subscribe(result -> log.info("Entity has been saved: {}", result));
     }
 }
